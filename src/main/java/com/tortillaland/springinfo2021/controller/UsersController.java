512eb11b68abe7df72104bed396204142cc6b3aa
Brian Alexis Schulz <brian-alexix@hotmail.com>
2021-12-13 17:26:24 -0300

    Modificando la estructura completa.
    CopyPaste de la estructura para relacionar entidades.

--- src/main/java/com/tortillaland/springinfo2021/controller/UsersController.java
+++ src/main/java/com/tortillaland/springinfo2021/controller/UsersController.java
@@ -1,69 +1,43 @@
 package com.tortillaland.springinfo2021.controller;
 
-import java.util.List;
-import java.util.Optional;
 import com.tortillaland.springinfo2021.dao.UsersDAO;
 import com.tortillaland.springinfo2021.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping(value = "/users")
 public class UsersController {
-    @Autowired
-    private UsersDAO userDAO;
-   
-    @GetMapping
-   public ResponseEntity<List<User>> getUser(){
-       List<User> users = userDAO.findAll();
-       return ResponseEntity.ok(users);
-   }
 
-    @RequestMapping(value="{userId}")
-   public ResponseEntity<User> getUserById(@PathVariable("userId") Long userId){
-       Optional<User> Optionalusers = userDAO.findById(userId);
-       if(Optionalusers.isPresent()){
-           return ResponseEntity.ok(Optionalusers.get());
-       }else{
-           return ResponseEntity.noContent().build();
-       }
-   }
-   @PostMapping
-   public ResponseEntity<User> createUser(@RequestBody User user){
-       User newUser = userDAO.save(user);
-       return ResponseEntity.ok(newUser);
-   }
-   @DeleteMapping(value="{userId}")
-   public ResponseEntity<Void> deleteUser(@PathVariable("userId") Long userId){
-       userDAO.deleteById(userId);
-       return ResponseEntity.ok(null);
-   }
-   @PutMapping
-   public ResponseEntity<User> updateUser(@RequestBody User user){
-       Optional<User> updateUser = userDAO.findById(user.getId());
-       if(updateUser.isPresent()){
-           User userUpdate = updateUser.get();
-           userUpdate.setName(user.getName());
-           userUpdate.setLastName(user.getLastName());
-           userUpdate.setEmail(user.getEmail());
-           userUpdate.setPassword(user.getPassword());
-           userUpdate.setCreationDate(user.getCreationDate());
-           userUpdate.setCity(user.getCity());
-           userUpdate.setProvince(user.getProvince());
-           userUpdate.setCountry(user.getCountry());
-           userUpdate.setType(user.getType());
-           userDAO.save(userUpdate);
-           return ResponseEntity.ok(userUpdate);
-       }else{
-           return ResponseEntity.notFound().build();
-       }
+    private final UsersDAO usersDAO;
+
+    public UsersController(UsersDAO usersDAO) {
+        this.usersDAO = usersDAO;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@Valid @RequestBody User user) {
+        return new ResponseEntity<>(usersDAO.save(user), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> obtenerTodos(
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fechaDesde) {
+        if (fechaDesde != null) {
+            List<User> user = usersDAO.findByCreatedDateAfter(fechaDesde.atStartOfDay());
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(usersDAO.findAll(), HttpStatus.OK);
     }
 }
--- src/main/java/com/tortillaland/springinfo2021/entity/User.java
+++ src/main/java/com/tortillaland/springinfo2021/entity/User.java
@@ -1,13 +1,21 @@
 package com.tortillaland.springinfo2021.entity;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.*;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name="users")
@@ -15,17 +23,40 @@ public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
+
+    @NotEmpty(message = "El nombre no puede ser vacio")
     private String name;
+
+    @NotEmpty(message = "El apellido no puede ser vacio")
     private String lastName;
+
+    @NotEmpty(message = "El apellido no puede ser vacio")
     @Email(regexp = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$")
     private String email;
+
+    @NotEmpty(message = "El password no puede ser vacio")
+    @Size(min = 8, max = 20)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
-    private LocalDate creationDate;
+
+    @CreationTimestamp
+    private LocalDate createdDate;
+
+    @NotEmpty(message = "La ciudad no puede ser vacio")
     private String city;
+
+    @NotEmpty(message = "La provincia no puede ser vacio")
     private String province;
+
+    @NotEmpty(message = "El pais no puede ser vacio")
     private String country;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
     private UserType type;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Emprend> emprends = new ArrayList<>();
 
     public Long getId() {
         return this.id;
@@ -67,12 +98,12 @@ public class User {
         this.password = password;
     }
 
-    public LocalDate getCreationDate() {
-        return this.creationDate;
+    public LocalDate getCreatedDate() {
+        return this.createdDate;
     }
 
-    public void setCreationDate(LocalDate creationDate) {
-        this.creationDate = creationDate;
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
     }
 
     public String getProvince() {
@@ -106,6 +137,5 @@ public class User {
     public void setType(UserType type) {
         this.type = type;
     }
-    
-    
+
 }
